ee26a2ea718b9099d0016b10c0f9d83dad4bcdd9
Víctor Felipe Godoy Hernández <vigohe@gmail.com>
2018-08-26 04:38:16 -0300

    1st commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM alpine:latest
+
+COPY ./var.sh /bin/
---
+++ var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Author: Christian Bargmann <chris@cbrgm.de>
+set -e
+
+# Assign default variables if not set
+PLUGIN_VAULT=${PLUGIN_VAULT}
+
+echo ${PLUGIN_VAULT}
